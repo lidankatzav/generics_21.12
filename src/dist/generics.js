cd3f3f4cab33d20799e7bab89f6cb649c8e4f1b0
lidankatzav <lidankatzav5@gmail.com>
2022-12-22 15:58:10 +0200

    full generics

--- src/dist/generics.js
+++ src/dist/generics.js
@@ -24,14 +24,44 @@ var PrimeProperty = /** @class */ (function () {
     };
     return PrimeProperty;
 }());
-var PalindromesProperty = /** @class */ (function () {
-    function PalindromesProperty() {
+var PalindromeProperty = /** @class */ (function () {
+    function PalindromeProperty() {
     }
-    PalindromesProperty.prototype.isProperty = function (element) {
-        return String(element) == String(element).split('').reverse().join('');
+    PalindromeProperty.prototype.isProperty = function (element) {
+        if (typeof element === 'string') {
+            return element == element.split('').reverse().join('');
+        }
+        if (typeof element === 'number') {
+            var reverse = 0;
+            var temp = element;
+            while (temp != 0) {
+                reverse = (reverse * 10) + (temp % 10);
+                temp = Math.floor(temp / 10);
+            }
+            return reverse == element;
+        }
     };
-    return PalindromesProperty;
+    return PalindromeProperty;
+}());
+var Person = /** @class */ (function () {
+    function Person(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    return Person;
+}());
+var PersonPalindromeProperty = /** @class */ (function () {
+    function PersonPalindromeProperty() {
+    }
+    PersonPalindromeProperty.prototype.isProperty = function (element) {
+        var palindromeProp = new PalindromeProperty();
+        return palindromeProp.isProperty(element.id);
+    };
+    return PersonPalindromeProperty;
 }());
 console.log(countElementsByProperty([1, 2, 3, 4, 5], new OddProperty));
 console.log(countElementsByProperty([1, 2, 3, 4, 5], new PrimeProperty));
-console.log(countElementsByProperty(['kayak', 'hello', 'madam', 'sing', 'redder', 'racecar'], new PalindromesProperty));
+console.log(countElementsByProperty(['kayak', 'hello', 'madam', 'sing', 'redder', 'racecar'], new PalindromeProperty));
+console.log(countElementsByProperty([new Person('Lidan', 12321), new Person('Or', 1451), new Person('Lior', 5)], new PersonPalindromeProperty));
+// Expected output -->
+//      3-3-4-2
--- src/generics.ts
+++ src/generics.ts
@@ -3,32 +3,75 @@ interface Property<T> {
 }
 
 function countElementsByProperty<T>(array: T[], prop: Property<T>): number {
+
     const arrAfterFilter = array.filter((value) => 
         prop.isProperty(value));
     return arrAfterFilter.length;
 }
 
 class OddProperty implements Property<number> {
+
     isProperty(element: number): boolean {
         return element % 2 !== 0;
     }
+
 }
 class PrimeProperty implements Property<number> {
+
     isProperty(element: number): boolean {
        for(let i = 2, s = Math.sqrt(element); i <= s; i++) {
         if(element % i === 0) return false; 
        }     
        return element > 1;
     } 
+
 }
 
-class PalindromesProperty implements Property<string | number> {
+class PalindromeProperty implements Property<string | number> {
+
     isProperty(element: string | number): boolean {
-        return String(element) == String(element).split('').reverse().join('');
+
+        if(typeof element === 'string') {
+            return element == element.split('').reverse().join('');
+        }
+
+        if(typeof element === 'number') {
+            let reverse = 0;
+            let temp = element;
+            while (temp != 0) {
+                reverse = (reverse * 10) + (temp % 10);
+                temp = Math.floor(temp / 10);
+            }
+            return reverse == element; 
+        }
+
+    }
+}
+class Person {
+
+    public name: string;
+    public id: number;
+
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+
+}
+
+class PersonPalindromeProperty implements Property<Person> {
+
+    isProperty(element: Person): boolean {
+        const palindromeProp = new PalindromeProperty();
+        return palindromeProp.isProperty(element.id);
     }
 }
 
-
 console.log(countElementsByProperty([1,2,3,4,5], new OddProperty));
 console.log(countElementsByProperty([1,2,3,4,5], new PrimeProperty));
-console.log(countElementsByProperty(['kayak', 'hello', 'madam', 'sing', 'redder', 'racecar'], new PalindromesProperty));
+console.log(countElementsByProperty(['kayak', 'hello', 'madam', 'sing', 'redder', 'racecar'], new PalindromeProperty));
+console.log(countElementsByProperty([new Person('Lidan', 12321), new Person('Or', 1451), new Person('Lior', 5)], new PersonPalindromeProperty));
+
+// Expected output -->
+//      3-3-4-2
+
